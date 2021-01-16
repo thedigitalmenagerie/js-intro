0f6fc752073c35f4d8df6ce0b50d3afef748b811
Honey-Rae Swan <honey-raeswan@Honey-Raes-MacBook-Air.local>
2021-01-16 11:08:04 -0600

    variables, data types, and math to js file

---
+++ main.js
@@ -0,0 +1,56 @@
+// console.log("CONNECTED");
+
+// Variables
+
+// let firstName = 'Beyonce';
+// let lastName = 'Knowles';
+
+//console.log(firstName + " " + lastName);
+//console.log(`First Name: ${firstName} Last Name: ${lastName}`);
+
+// firstName = 'Dr.T';
+// lastName = 'Vasquez';
+
+// console.log(firstName + lastName);
+// console.log(`First Name: ${firstName} Last Name: ${lastName}`);
+
+// const favoriteFood ='tacos';
+// console.log(favoriteFood);
+
+// favoriteFood = 'Gumbo';
+// console.log(favoriteFood);
+
+
+// End Variables
+
+// Date Types
+
+//const dog = 'bark';
+//console.log(typeof dog);
+
+// const answer = 33;
+//const negative = -14;
+//console.log(typeof answer, typeof negative);
+
+//const iLoveJavaScript = true;
+//console.log(typeof iLoveJavaScript);
+
+// End Data Types
+
+// Start Math
+
+//console.log(Math.pow(2,2));
+//console.log(Math.pow(3,2));
+//console.log(Math.pow(3,3));
+
+//console.log(Math.round(6.5));
+//console.log(Math.round(6.45));
+
+//console.log(Math.floor(6.9999));
+
+//console.log(Math.ceil(6.0001));
+
+// const cat = "kitty";
+// console.log(cat.length); // lengths start at one but index starts at 0
+
+//End Math
