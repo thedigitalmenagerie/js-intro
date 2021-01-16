d46e6381223430de196315814f11063f6b25ed7b
Honey-Rae Swan <honey-raeswan@Honey-Raes-MacBook-Air.local>
2021-01-16 12:36:55 -0600

    funtion notes

---
+++ main.js
@@ -0,0 +1,37 @@
+// Start Functions
+
+// function sayHello() {
+//  console.log('Hello!');
+// } this way can have it called before it is initialized
+ 
+//const sayHello = function() {
+//  console.log('Hello!');
+// }
+
+// const sayHello = () => {
+//  console.log('Hello!');
+// } this and the previous way are more up-to-date ways of initializing functions
+
+// sayHello();
+
+// End Functions
+
+// Start Functions with Arguments
+
+//function logsHello(name) {
+//  console.log(`Hello, ${name} !`);
+//}
+
+//logsHello(`Honey-Rae`);
+
+// const sum = 55;
+
+// const addsTwoNumbers = (num1, num2) => {
+//  const sum = num1 + num2;
+//  console.log(sum);
+//  return sum;
+//}
+
+//console.log(addsTwoNumbers(5, 7)); // returns undefined because it is not returned 
+//console.log(sum); // outside of scope of function and totally disregards contents of the function
+// End Functions with Arguments
